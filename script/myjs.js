ca8f905634d0d6d68456e808ce70342eacc97f9d
Benjamin Varghese <varghese.benjamin09@gmail.com>
2018-03-26 22:08:02 +1100

    first commit

---
+++ script/myjs.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    $( "h1" ).click(function( event ) {
+        alert( "The link will no longer take you to jquery.com" );
+        $(this).hide("slow");        
+    });
+    $( "a" ).click(function( event ) {
+        alert( "The link will no longer take you to jquery.com" );
+        event.preventDefault();
+    });
+    $("a").addClass("anchor");
+    $("a").removeClass("anchor");
+});
